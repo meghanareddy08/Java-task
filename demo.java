fa6c07b317da0c2467ee09a4e96173c188c90bd1
meghanareddy08 <152237078+meghanareddy08@users.noreply.github.com>
2025-01-06 19:11:59 -0600

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,10 +1,10 @@
-public static DEMO{
-	public static void main(String[] args){
-		init rollno = 1596;
-		String sname="Meghana";
-		String coursename="Python";
-		System.out.println("Learn here and lead anywhere");
-		System.out.println("My name is:" +sname);
-		System.out.println("Thankyou");
-	}
+public class DEMO {
+    public static void main(String[] args) {
+        int rollno = 1596; // Corrected data type
+        String sname = "Meghana";
+        String coursename = "Python";
+        System.out.println("Learn here and lead anywhere");
+        System.out.println("My name is: " + sname);
+        System.out.println("Thank you");
+    }
 }
